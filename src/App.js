de974853bfc2160aa41709e6e59f0348f8a2017f
Alper Demir <105297896+Hololopoto@users.noreply.github.com>
2024-02-08 01:18:51 +0300

    Stil değişiklikleri: App.js güncellendi

--- src/App.js
+++ src/App.js
@@ -48,23 +48,23 @@ function App() {
 
   console.log("POKEDATA", poke);
   return (
-    <div className="mb-20 px-40">
+    <div className="mb-20 px-40 max-[768px]:px-10">
       <div className="search my-11 flex flex-col items-center gap-6 w-full justify-center">
         <h1 className="text-3xl text-bold"> Alper </h1>
         <h1 className="text-3xl text-bold"> || POKEDEX || </h1>
         <input
-          className="border-black w-1/2 border-4 rounded-md "
+          className="border-black w-1/2 border-4 rounded-md max-[768px]:w-[100%]"
           type="search"
           name="PokeSearch"
           id=""
         />
       </div>
-      <div className="flex gap-11 mx-11 flex-wrap flex-row">
+      <div className="flex gap-11 mx-1 flex-wrap flex-row">
         {poke.map((x, index) => {
           return (
             <div
               key={index}
-              className="pokes flex-row items-center justify-center border-black border-4 w-[16%] rounded-lg  flex">
+              className="pokes flex-row items-center justify-center border-black border-4 xl:w-[20%] lg:w-[26%] max-[768px]:w-[100%] rounded-lg  flex">
               <div className="flex-col my-6">
                 <img
                   className="w-[200px] hover:scale-125 transition h-auto"
@@ -73,7 +73,7 @@ function App() {
                     .padStart(3, "0")}.png`}
                   alt="PokemonPhoto"
                 />
-                <div className="poke-infos text-center py-3 gap-1 grid">
+                <div className="poke-infos text-lg text-center py-3 gap-1 grid">
                   <div className=" ">
                     {x.name[0].toUpperCase() + x.name.slice(1)}
                   </div>
@@ -85,7 +85,7 @@ function App() {
                       x.types[0].type.name[0].toUpperCase() +
                       x.types[0].type.name.slice(1)}
                   </div>
-                  <div className="poke-weight">{x.weight + "Kg"}</div>
+                  <div className="poke-weight">{x.weight + " Kg"}</div>
                 </div>
               </div>
             </div>
